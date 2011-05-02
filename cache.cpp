d59d33119e6ea17035331e81461826990625c563
vasia <vasilikikalavri@gmail.com>
2011-05-02 18:35:07 +0200

    read hit/miss/dirty :)

--- cache.cpp
+++ cache.cpp
@@ -32,11 +32,11 @@ void Cache::Reset()
 	//set all dirty bits to 0
 	int i;
 	for(i=0; i<clines; i++){
-		dirty_bit_buffer[i] = '0';
+		dirty_bit_buffer[i] = '1';
 		//buffer[i] = -cline_size;
 		buffer[i] = i*cline_size;
 	}
-
+	
 	hits = 0;
 	misses = 0;
 }
@@ -47,22 +47,32 @@ void Cache::Start()
 	mdata_t item;
 	
 	//send data to the processor
-	if (Sim::cycle >= respond) {
-		item.type = 'W';	//doesn't make sense (before you ask)
+	if(Sim::cycle >= respond){
+		item.type = 'R';	//does it make sense now? (before you ask)
 		item.addr = respond_addr;
 		printf("C(s): sending %lu\n", respond_addr);
 		p_out.data = item;
 		respond = NEVER;
 	}
 
-	//send data to the memory
-	else if (miss && !wait && !m_in.busy && miss_item.type == 'R') {
+	//send data to the memory (read miss case)
+	else if(miss && !wait && !m_in.busy && miss_item.type == 'R'){
 		printf("C: miss item = %c %lu\n", miss_item.type, miss_item.addr);
 		//send the item to the output port
 		m_out.data = miss_item;
 		wait = 1;
 		wait_addr = miss_item.addr;
 		printf("C: wait = %d, wait_addr = %lu\n", wait, wait_addr);
+	}
+
+	//send data to the memory (replacing a dirty block)
+	else if(write_back && !wait && !m_in.busy){
+		printf("C: sending dirty item %c %lu to memory\n", dirty_item.type, dirty_item.addr);
+		//send item to the output port
+		m_out.data = dirty_item;
+		write_back = 0;
+		//set the respond, so we can reply to the processor
+		respond = Sim::cycle + cycles_t(latency);
 	}
 	
 }
@@ -85,18 +95,31 @@ void Cache::End()
 		//data received -> don't wait anymore
 		wait = 0;
 		miss = 0;
+		pos = (int)((item.addr/cline_size)%clines);	
 
 		//check dirty bit
+		if(dirty_bit_buffer[pos] == '1'){
+			printf("C: Dirty block!\n"); 
+			//need to replace a dirty block
+			//so, we need to send it back to memory (write-back policy)
+			dirty_item.addr = buffer[pos];
+			dirty_item.type = 'W';
+			dirty_item.size = 4;
+			write_back = 1;					
+		}
+		else{
+			//update the cache contents
+			buffer[pos] = (item.addr/cline_size)*cline_size;
+			printf("C: Cache updated at position %d with address %lu\n", pos, buffer[pos]);
 
-		//update the cache contents
-		pos = (int)((item.addr/cline_size)%clines);	
-		buffer[pos] = (item.addr/cline_size)*cline_size;
-		printf("C: Cache updated at position %d with address %lu\n", pos, buffer[pos]);
+			//reset dirty bit
+			dirty_bit_buffer[pos] = '0';
 
-		//now I have the data and I can reply to the processor
-		respond = Sim::cycle + cycles_t(latency);
-		respond_addr = item.addr;
-		std::cout << "C: respond = " << respond << "\n";
+			//now I have the data and I can reply to the processor
+			respond = Sim::cycle + cycles_t(latency);
+			respond_addr = item.addr;
+			std::cout << "C: respond = " << respond << "\n";
+		}
 	}
 
 	//the processor sent a request to the cache
@@ -123,17 +146,19 @@ void Cache::End()
 				printf("C: It's a miss! %lu\n", item.addr);
 				miss = 1;
 				miss_item = item;
+				//only setting the respond address because we don't know how long it will take the memory to answer
+				respond_addr = item.addr;
 			}
 		}
 	}
 
-	if ((respond != NEVER) || miss) {
+	if ((respond != NEVER) || miss || write_back) {
 		p_out.busy = true;
 	} else {
 		p_out.busy = false;
 	}
 
-	if(miss){
+	if(miss || write_back){
 		setNextCycle(Sim::cycle + cycles_t(1));
 	}
 	else{
